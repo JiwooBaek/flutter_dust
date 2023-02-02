1a8b1feef7d6e1c91adf93bac2e8836589490f6e
baekjiwoo <20172049@sungshin.ac.kr>
2023-02-02 21:27:30 +0900

    레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -40,41 +40,66 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('현재 위치 미세먼지', style: TextStyle(fontSize: 30),),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text('얼굴사진'),
-                      Text('80'),
-                      Text('보통'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('얼굴사진'),
-                      Text('80'),
-                      Text('보통'),
-                    ],
-                  )
-                ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('현재 위치 미세먼지', style: TextStyle(fontSize: 30),),
+              SizedBox(
+                height: 16,
               ),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50)),
-                  onPressed: () {},
-                  child: Icon(Icons.refresh)),
-            )
-          ],
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text('얼굴사진'),
+                          Text('80', style: TextStyle(fontSize: 40),),
+                          Text('보통', style: TextStyle(fontSize: 20),),
+                        ],
+                      ),
+                      color: Colors.yellow,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Text('이미지'),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Text('11°', style: TextStyle(fontSize: 16),)
+                            ],
+                          ),
+                          Text('습도 100'),
+                          Text('풍속 5.7m/s'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50)),
+                    onPressed: () {},
+                    child: Icon(Icons.refresh)),
+              )
+            ],
+          ),
         ),
       ),
     );
