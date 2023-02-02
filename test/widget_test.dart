c113e34d92b612d6ab40fc9b6a563b5f884dcb6c
baekjiwoo <20172049@sungshin.ac.kr>
2023-02-03 00:19:15 +0900

    API 적용

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,19 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dust/models/AirResult.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_dust/main.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('http 통신 테스트', () async{
+    var response = await http.get(Uri.parse('https://api.airvisual.com/v2/nearest_city?key=494ddf20-d03a-4f9d-8d28-f767bd3497c9'));
+    expect(response.statusCode, 200);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    expect(result.status, 'success');
   });
 }
